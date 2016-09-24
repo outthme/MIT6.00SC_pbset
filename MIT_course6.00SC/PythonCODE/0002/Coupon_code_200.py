85c9bd1902d5ab3ad0c51a36141c430255ddeaac
Joffrey <swmhzx@gmail.com>
2016-09-24 20:01:22 +0800

    Python exercise 0003

--- MIT_course6.00SC/PythonCODE/0002/Coupon_code_200.py
+++ MIT_course6.00SC/PythonCODE/0002/Coupon_code_200.py
@@ -5,6 +5,7 @@ Spyder Editor
 This is a temporary script file.
 """
 import md5
+import MySQLdb
 def coupon_gen(amounts):
     key_list=[]
     for i in range(0, amounts-1):
@@ -18,3 +19,15 @@ for i in key_accept:
     m.update(i)
     key_dic[m.hexdigest()] = 0
 
+#use MySQLdb.connect to create a connection and create a cursor
+conn = MySQLdb.connect(db='testbase', host='localhost', user='testuser',passwd='225157aa')
+cursor = conn.cursor()
+
+#put the key data which from key_dic into the connection path 
+for i in key_dic:
+    cursor.execute("INSERT INTO coupon(coupon_keys) VALUES('" + i +"')")
+    print cursor.fetchall()
+
+cursor.close()
+conn.commit() #Be attention to commit your affairs
+conn.close()
